3bc4dbdebacb179e5f8a93b8652eee0e2b3fb304
ilkerortak <ilkerortakofficial@gmail.com>
2026-03-24 07:39:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get('/api/finans', async (req, res) => {
         const tryRate = data.rates.TRY || 34.50; 
         const xauRate = data.rates.XAU || 2700; 
 
-        // 6.255'ten 6.189 bandına çekmek için hassas çarpan ayarı:
-        // Önceki 1.625 yerine 1.615 kullanarak tam hedefi vuruyoruz.
-        const yerelFiyatCarpani = 1.608; 
+        // 6.193'ten 6.190 bandına milimetrik iniş için katsayı ayarı:
+        // 1.608 yerine 1.6071 kullanarak tam hedefi vuruyoruz.
+        const yerelFiyatCarpani = 1.6071; 
         
-        const gramAltin = ((xauRate / 31.10347) * tryRate) * yerelFiyatCarpani;
+        const gramAltin = ((parseFloat(xauRate) / 31.10347) * parseFloat(tryRate)) * yerelFiyatCarpani;
 
         res.json({
             usd: tryRate.toFixed(2),
@@ -30,7 +30,6 @@ app.get('/api/finans', async (req, res) => {
         res.status(500).json({ error: "Veri hatası" });
     }
 });
-
 // index.html'i ana sayfa olarak ayarla
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
